398d9575d8037be54059dee2895e58ccdc6472e3
Daniel Yonev <daniel.yonev@gmail.com>
2025-07-18 14:28:32 +0300

    initial project

---
+++ .mocharc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+ // reporter: 'node_modules/mochawesome',
+  spec: 'testSuite/**.js',
+  'reporter-option': [
+    'overwrite=false',
+    'reportTitle=Selenium WD:test',
+    'reportFilename=[status]_[datetime]-[name]-report',
+    'timestamp=longDate',
+    'showPassed=true',
+    'json=true',
+    'code=true',
+    'charts=true',
+    'autoOpen=true',
+    'inline=true',
+    'showHooks=true',
+    'help=true',
+  ],
+};
+console.log("Loaded .mocharc.cjs configuration");
---
+++ helpers/driver.js
@@ -0,0 +1,24 @@
+import { Builder } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
+
+export let driver;
+
+export default async function createDriver() {
+  const options = new chrome.Options();
+
+  // Add useful arguments
+  options.addArguments(
+    '--disable-search-engine-choice-screen',
+    '--disable-popup-blocking',
+    '--disable-notifications',
+    '--disable-infobars',
+    '--disable-blink-features=AutomationControlled',
+    '--start-maximized'
+
+  );
+    // Return new WebDriver instance with options
+  return await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(options)
+    .build();
+}
