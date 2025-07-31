c4c36dd70634bfad7e0d08544fb0d4968ee92ff0
Daniel Yonev <daniel.yonev@gmail.com>
2025-07-31 12:19:51 +0300

    Test case 1 - successful login, done

--- helpers/locators.js
+++ helpers/locators.js
@@ -1,3 +1,5 @@
-import { Builder, By, Key, until } from 'selenium-webdriver';
+import { By } from 'selenium-webdriver';
 
 export const loginButton = By.xpath("//input[@id = 'login-button']");
+export const shoppingCartIcon = By.xpath("//div[@class = 'primary_header']//div[@id = 'shopping_cart_container']");
+export const headerText = By.xpath("//div[@class = 'login_logo']")
--- testSuite/Test case 1 - Successful login.js
+++ testSuite/Test case 1 - Successful login.js
@@ -1,8 +1,8 @@
 import createDriver from '../helpers/driver.js';
 import { Builder, By, Key, until } from 'selenium-webdriver';
-import { loginButton } from '../helpers/locators.js'
-import chai from 'chai';
-import expect from 'chai';
+import { headerText, loginButton, shoppingCartIcon } from '../helpers/locators.js'
+import chai, { assert } from 'chai';
+import { expect } from 'chai';
 
 
 let driver;
@@ -15,28 +15,32 @@ describe('Positive test: Successful login',async function () {
 
   it('Test case 1: Open browser and navigate', async function() {
     await driver.get('https://www.saucedemo.com');
-    console.log("Page loaded successfully!")
-
+    let logo = driver.findElement(headerText)
+    await driver.wait(until.elementIsVisible(logo),5000);
   });
 
-  it('Test case 2: Enter username: standard_user', async function(){
+  it('Test case 2: Enter username: standard_user', async function( ){
     let username_field = await driver.findElement(By.xpath("//div[@class = 'form_group']//input[@placeholder = 'Username']"));
     await driver.wait(until.elementIsVisible(username_field), 5000);
     await username_field.sendKeys("standard_user");
   })
 
-  it('Test case 3: Enter password: secret_sauce', async function(){
+  it('Test case 3: Enter password: secret_sauce', async function() {
     let password_field = await driver.findElement(By.xpath("//div[@class = 'form_group']//input[@placeholder = 'Password']"));
     await driver.wait(until.elementIsVisible(password_field), 5000);
     await password_field.sendKeys("secret_sauce");
 })
-  it('Test case 4: Locate "login" button and click', async function(){
+  it('Test case 4: Locate "login" button and click', async function() {
     let button = await driver.findElement(loginButton);
     await button.click();
   })
 
+  it('Test case 5: Assert successful login', async function () {
+    let icon = await driver.findElement(shoppingCartIcon);
+    await driver.wait(until.elementIsVisible(icon), 5000);
+  })
+
   after(async function() {
     await driver.quit();
-
-  });
+   });
   })
