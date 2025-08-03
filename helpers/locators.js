4c4f219288a5c160f80263fdec29923972f7e017
Daniel Yonev <daniel.yonev@gmail.com>
2025-08-03 13:40:46 +0300

    a few opitimizations and a locator function in 'locators.js'

--- helpers/locators.js
+++ helpers/locators.js
@@ -1,5 +1,25 @@
-import { By } from 'selenium-webdriver';
+import { By, until } from 'selenium-webdriver';
+export let driver;
 
-export const loginButton = By.xpath("//input[@id = 'login-button']");
-export const shoppingCartIcon = By.xpath("//div[@class = 'primary_header']//div[@id = 'shopping_cart_container']");
-export const headerText = By.xpath("//div[@class = 'login_logo']")
+export const landingPage = {
+
+    loginButton: "//input[@id = 'login-button']",
+    headerText: "//div[@class = 'login_logo']",
+    username_field:"//div[@class = 'form_group']//input[@placeholder = 'Username']",
+    password_field: "//div[@class = 'form_group']//input[@placeholder = 'Password']"
+
+};
+
+export const homePage = {
+
+     shoppingCartIcon:"//div[@class = 'primary_header']//div[@id = 'shopping_cart_container']"
+
+};
+
+export const locateAndAssertElement = async (driver, element) => {
+
+    let temp = await driver.findElement(By.xpath(element));
+    await driver.wait(until.elementIsVisible(temp),5000);
+    await console.log("Element located successfully: ", element);
+
+  }
