48486e6bd35418d21d4b38586059bec787f6c448
Daniel Yonev <daniel.yonev@gmail.com>
2025-08-28 14:57:04 +0300

    new test case

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   //reporter: 'node_modules/mochawesome',
-  spec: 'test_cases/**/**.js',
+  spec: 'test_cases/**/03.Successful_order.js',
   'reporter-option': [
     'overwrite=false',
     'reportTitle=Selenium WD:test',
--- test_cases/02.shopping_cart_menu/03.Successful_order.js
+++ test_cases/02.shopping_cart_menu/03.Successful_order.js
@@ -1,6 +1,6 @@
 import createDriver from '../../helpers/driver.js';
 import { By, until } from 'selenium-webdriver';
-import { navigateToHomePage, homePage, shoppingCartPage, locateAndClickElement } from '../../helpers/locators.js';
+import { navigateToHomePage, homePage, shoppingCartPage, locateAndClickElement, locateAndAssertElement, checkoutInformationPage, overviewInformationPage } from '../../helpers/locators.js';
 import assert from 'assert';
 import { loginFunction } from '../../helpers/login.js';
 
@@ -34,9 +34,48 @@ describe('Successful order', async function () {
   })
 
   it("Test case 6: 'Checkout: Your information' - Input 'First Name'", async function(){
-    
+      const firstName = await driver.findElement(By.xpath(checkoutInformationPage.firstNameField));
+      await driver.wait(until.elementIsVisible(firstName), 5000);
+      await firstName.sendKeys("TestFirstName");
   })
 
+  it("Test case 7: 'Checkout: Your information' - Input 'Last Name'", async function(){
+      const lastName = await driver.findElement(By.xpath(checkoutInformationPage.lastNameFIeld));
+      await driver.wait(until.elementIsVisible(lastName), 5000);
+      await lastName.sendKeys("TestLastName");
+  }) 
+
+  it("Test case 8:  'Checkout: Your information' - Input 'Zip'", async function(){
+      const zipCode = await driver.findElement(By.xpath(checkoutInformationPage.zipCodeField));
+      await driver.wait(until.elementIsVisible(zipCode), 5000);
+      await zipCode.sendKeys("0000");
+  })
+
+  it("Test case 9: Press continue", async function(){
+    await locateAndClickElement(driver, checkoutInformationPage.continueButton);
+  })
+
+  it("Test case 10: Press 'Finish' to complete order", async function(){
+    await locateAndClickElement(driver, overviewInformationPage.finishButton);
+  })
+
+  it("Test case 11: Check if 'Thank you for your order!' message is visible", async function(){
+    const text = await driver.findElement(By.xpath("//div[@id='checkout_complete_container']//h2[contains(.,'Thank you for your order!')]"))
+    await driver.wait(until.elementIsVisible(text), 5000);
+  })
+
+  it("Test case 12: Check if 'Back Home' button is visible and click it", async function(){
+    const backButton = await driver.findElement(By.xpath("//div[@id='checkout_complete_container']//button[@id='back-to-products']"));
+    await driver.wait(until.elementIsVisible(backButton), 5000);
+    await backButton.click();
+  })
+
+  it("Test case 13: Check if we are redirected to the home page", async function(){
+    const productsHeader = await driver.findElement(By.xpath("//span[contains(.,'Products')]"));
+    await driver.wait(until.elementIsVisible(productsHeader), 5000);
+    const visible = await productsHeader.isDisplayed();
+    assert.strictEqual(visible, true, "Element is visible, we are on the home page");
+  })
 
   after(async function() {
     await driver.quit();
